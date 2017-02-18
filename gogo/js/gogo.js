819ae7d6ca6114fbaf3b59f89f3f6c0261fded78
Roman Tronenko <roman@italki.com>
2017-02-18 14:12:34 +0800

    /gogo +more tags  +contact me

--- gogo/js/gogo.js
+++ gogo/js/gogo.js
@@ -79,9 +79,17 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
-    $('ul.tags').randomize();
-    $('ul.tags li').hide().slice(0, 9).show();
-    $('ul.tags').fadeIn().css("display","inline-block");
+    function randomizeTagsAndAddMore() {
+          $('ul.tags').randomize();
+          $('ul.tags li').hide().slice(0, 8).show();
+          $('ul.tags').append('<li><a href="#more_tags" class="more_tags">more</a></li>');
+          $('ul.tags').fadeIn().css("display","inline-block");
+          $('.more_tags').click(function() {
+            randomizeTagsAndAddMore();
+          });
+    }
+    randomizeTagsAndAddMore();
+
 
     // $(".fadeIn").each(function() {
     //      var src = $(this).data("src");
@@ -98,4 +106,7 @@
     //
     //      }
     //  });
+    $('#contact_form_toggle').click(function () {
+      $('#contact_form').toggle(); //this will hide the fullscreen div if you click away from the image.
+    });
 })(jQuery); // End of use strict
